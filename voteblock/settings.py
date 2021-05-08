9d8690c40a161010d0e0f3020e2eb1b4b1cfa5f3
vicky-hemnani <58837151+vicky-hemnani@users.noreply.github.com>
2021-05-08 10:35:05 +0530

    Update settings.py

--- voteblock/settings.py
+++ voteblock/settings.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 EMAIL_ADDRESS = 'voting.app2000@gmail.com'
 EMAIL_PASSWORD = 'vhemnani99'
